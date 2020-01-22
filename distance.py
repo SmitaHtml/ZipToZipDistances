058351905588f6a5945f2741f46ab621c348f70b
Smita <smitathengne@yahoo.com>
2020-01-21 23:45:33 -0500

    removed print lines

--- distance.py
+++ distance.py
@@ -1,5 +1,6 @@
 import geopy
 import geopy.distance
+import sys
 
 def distance(fromZip, fromlat, fromlong, toZip, tolat, tolong, fileziptozipdistance):
 
@@ -10,19 +11,16 @@ def distance(fromZip, fromlat, fromlong, toZip, tolat, tolong, fileziptozipdista
     #coords_2 = (tolat, tolong)
 
     try:
-        ziptozipdistance = geopy.distance.geodesic(coords_1, coords_2).miles
-        #successfull distance calculation
-        print ("                From zip: ", fromZip, " To zip: ", toZip, "Distance is: ", ziptozipdistance)
-        fileziptozipdistance.write(str(fromZip) + ", " + str(toZip) + ", " + str(ziptozipdistance) + "\n")
-
+        if fromlat < -90 or fromlat > 90 or tolat < -90 or tolat > 90:
+            errorrow = str(fromZip) + ", " + str(toZip) + ", " + "Error: Latitude must be in the [-90; 90] range." + "\n"
+            fileziptozipdistance.write(errorrow)
+        else:
+            ziptozipdistance = geopy.distance.geodesic(coords_1, coords_2).miles
+            fileziptozipdistance.write(str(fromZip) + ", " + str(toZip) + ", " + str(ziptozipdistance) + "\n")
     except ValueError:
         #error in distance calculation
-        if fromlat < -90 or fromlat > 90:
-            print ("                From zip: ", fromZip, " To zip: ", toZip, "Error: Latitude must be in the [-90; 90] range.")
-            fileziptozipdistance.write(fromZip, toZip, "Error: Latitude must be in the [-90; 90] range." + "\n")
-        else:
-            print ("                From zip: ", fromZip, " To zip: ", toZip, "Error: value error.")
-            fileziptozipdistance.write(fromZip, toZip, "Error: value error."+ "\n")
+            valueerrorrow = str(fromZip) + ", " + str(toZip) + ", " + "Error: value error." + "\n"
+            fileziptozipdistance.write(valueerrorrow)
 
 if __name__ == '__main__':
     distance()
--- tozipdistances.py
+++ tozipdistances.py
@@ -1,9 +1,9 @@
 import csv
 import distance
 
-def tozipdistances(fromZip, fromlat, fromlong, fileziptozipdistance):
+def tozipdistances(file_name, fromZip, fromlat, fromlong, fileziptozipdistance):
 
-    with open('zipcodes0.csv') as csv_file_inner:
+    with open(file_name) as csv_file_inner:
         csv_reader_inner = csv.reader(csv_file_inner, delimiter=',')
         line_count = 0
         for row in csv_reader_inner:
@@ -14,7 +14,7 @@ def tozipdistances(fromZip, fromlat, fromlong, fileziptozipdistance):
                 toZip = row[0]
                 tolat = float(row[1])
                 tolong = float(row[2])
-                print('         ', line_count,'ToZip: ', toZip, tolat, tolong);
+                #print('         ', line_count,'ToZip: ', toZip, tolat, tolong);
                 distance.distance(fromZip, fromlat, fromlong, toZip, tolat, tolong, fileziptozipdistance)
                 line_count += 1
 
