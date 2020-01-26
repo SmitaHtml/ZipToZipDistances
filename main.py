d0929636d2e074bc4c91c938b1fccd86bfbeac35
Smita <smitathengne@yahoo.com>
2020-01-25 19:09:02 -0500

    merged code into one single file

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 import csv
-import tozipdistances
+import geopy
+import geopy.distance
 
-open('ziptozipdistancefile.txt', 'w').close();
-file_name = 'zipcodes0.csv';
-input_file_from = csv.DictReader(open(file_name))
+try:
 
-line_count = 1
-fileziptozipdistance = open("ziptozipdistancefile.txt", "a")
-fileziptozipdistance.write("fromZip, toZip, distanceInMiles\n")
-for row in input_file_from:
-    fromZip = row["zip"]
-    fromlat = float(row["lat"])
-    fromlong = float(row["long"])
-    print (str(line_count) + " From: " + str(row))
-    tozipdistances.tozipdistances(file_name, fromZip, fromlat, fromlong, fileziptozipdistance)
-    line_count += 1
-fileziptozipdistance.close();
+    infile_name = "zipcodes0.csv";
+    open_fromzip_infile = csv.DictReader(open(infile_name));
+
+    outfile_name = "outfile.csv";
+    open(outfile_name, 'w').close(); # deletes output file content
+    open_outfile = open(outfile_name, "a");
+    open_outfile.write("fromZip, fromLat, fromLong, toZip, toLat, toLong, distanceBetweenZipsInMiles\n")
+
+    from_line_count = 1
+    for fromziprow in open_fromzip_infile:
+        fromzip = fromziprow["zip"]
+        fromlat = fromziprow['lat']
+        fromlong = fromziprow['long']
+        print(str(from_line_count) + " " + str(fromzip) + ",  " + str(fromlat) + ",  " + str(fromlong));
+        open_tozip_infile = csv.DictReader(open(infile_name));
+        to_line_count = 1
+
+        for toziprow in open_tozip_infile:
+            tozip = toziprow["zip"]
+            tolat = toziprow['lat']
+            tolong = toziprow['long']
+            print("     " + str(to_line_count) + " " + str(tozip) + ",  " + str(tolat) + ",  " + str(tolong));
+
+            coords_1 = (fromlat, fromlong)
+            coords_2 = (tolat, tolong)
+            try:
+                ziptozipdistance = geopy.distance.geodesic(coords_1, coords_2).miles #calculates distance between two coordinates with lat and long
+                if ziptozipdistance < 200:
+                    print ("            From zip: ", fromzip, " To zip: ", tozip, " Distance is: ", ziptozipdistance, "\n")
+                    outputrow = str(fromziprow['zip'] + ", " + fromziprow["lat"] + ", " + fromziprow["long"] + ", " + toziprow['zip'] + ", " + toziprow["lat"] + ", " + toziprow["long"] + ", " + str(ziptozipdistance))
+                    open_outfile.write(outputrow + "\n");
+            except Exception as e:
+                #print("Exception encountered: {}".format(e))
+                print("           " + str(fromzip) + ", " + str(tozip) + ", " + " Error encountered: {}".format(e) + "\n")
+                outputrow = str(fromziprow['zip'] + ", " + fromziprow["lat"] + ", " + fromziprow["long"] + ", " + toziprow['zip'] + ", " + toziprow["lat"] + ", " + toziprow["long"]) + ", " + " Error encountered: {}".format(e)
+                open_outfile.write(outputrow + "\n")
+                pass
+
+            to_line_count += 1
+        from_line_count += 1
+
+except Exception as e:
+    print("Exception encountered: {}".format(e))
